08366310e387f156c74f4d4005a7d87355bcf9dd
lmussina-vass <lmussina@vassar.edu>
2026-02-21 19:39:08 -0500

    Add files via upload

---
+++ Stimulus_B.js
@@ -0,0 +1,231 @@
+// TODO LIST
+// - remove the [ ] around each story
+// - last sentence should end with ^ instead of ^
+// - add condition_b list at the bottom.
+
+
+const story_1B = 
+    `Not many people know that James Madison was a lover of poetry.^
+    He attended what is now known as Princeton University in New Jersey.^
+    Princeton University had many secret societies for students at the time.^
+    Some of these secret societies were dedicated to the practice of debate.^
+    These debate societies often had fierce rivalries with other debate societies.^
+    Meetings between rival groups sometimes extended beyond debates.^
+    Madison's society got involved in a poetry battle with another society.^
+    These poetry battles in the halls of Princeton were called paper wars.^
+    Some of Madison's poems from these paper wars have survived.^
+    One of the more famous poems depicts Madison's rival on a mythic quest.^
+    The quest ends with the rival getting beaten up by the Greek gods.^
+    Madison's strong writing skills were apparent even during his college years.^
+    Perhaps he drew upon these poetic talents when writing the Constitution.^`
+
+
+const story_2B = 
+    `Pam had a wide-brimmed red hat that she liked to wear to the park.^
+    She picked out red shoes and a red belt to match her red hat.^
+    The wind was strong when she went to the park with her friend Dan.^
+    The wind started to pick up and a sudden gust blew the hat off her head.^
+    Dan took off running in the direction of the hat and tried to grab it.^
+    The park had a lot of hills and Dan became tired very quickly.^
+    It seemed like he would never be fast enough to catch up to it.^
+    A stranger walking their dog noticed the hat blowing in the wind.^
+    The dog got away from its owner and took off running towards the hat.^
+    The dog got the hat and brought it back to its owner like a game of fetch.^
+    Dan was able to retrieve the hat from the dog-walking stranger.^
+    He brought the hat back to Pam and told her about the dog that caught it.^
+    Pam put the hat in her tote bag and vowed not to wear it in the wind again.^`
+
+
+const story_3B = 
+    `Joey had been fascinated by the game of chess since he was a little boy.^
+    He grew up in New York City and spent a lot of time in Central Park.^
+    There were groups of men who played chess together in the park.^
+    Joey often watched them with fascination until his mother made him leave.^
+    He learned the rules of the game without ever being formally taught.^
+    Watching other people play gave him lots of insight on strategy.^
+    He learned as much from their mistakes as he did from their victories.^
+    Joey eventually worked up the courage to challenge someone to a match.^
+    His opponent was a very skilled and strategic player and swiftly beat Joey.^
+    He told Joey that he had a lot of potential and a great mind for chess.^
+    Joey practiced against himself by playing both sides of a board equally fair.^
+    He returned to the park and challenged his old opponent to a rematch.^
+    Joey was able to hold his own and won the match in thirty moves.^`
+
+
+const story_4B = 
+    `Matthew was a college student studying Biology and Chemistry at Yale.^
+    He also liked running and was a star athlete on the cross-country team.^
+    He liked to travel a lot and studied abroad in Australia for a semester.^
+    Many of his friends in the biology department wanted to be doctors.^
+    Matthew was not sure he wanted to go to medical school after college.^
+    He thought it was expensive and he didn't think he'd be a good doctor.^
+    His professor told him to think about what he loved about science.^
+    Matthew's favorite science classes were about plant and animal biology.^
+    He did a lot of field research for some ecology and wildlife programs.^
+    He volunteered with an animal sanctuary during his semester in Australia.^
+    Fish were his favorite animal to study and he loved his field research.^
+    He shifted his focus to ecology and planned for a career in fisheries.^
+    He hoped to spend his career working to combat water pollution.^`
+
+
+const story_5B = 
+    `Spencer Silver is widely credited with inventing the Post-It note.^
+    He was a scientist with a goal of creating a strong adhesive glue.^
+    His famous invention began in his lab in Scotland in the 1960's.^
+    The adhesive he developed wasn't as strong as he thought it would be.^
+    He worried he had created something useless that people wouldn't need.^
+    One of his church friends mentioned a problem with her hymn book.^
+    The bookmarks she used always fell out when she turned the pages.^
+    Spencer put some of his adhesive on a bookmark and stuck it to a page.^
+    The adhesive was so weak that it didn't cause any damage to the page.^
+    Spencer's lab soon turned into one of the biggest factories in Canada.^
+    His new Post-It notes were immediately in high demand everywhere.^
+    People also used them to put notes on their mirrors and walls for reminders.^
+    Post-It notes are still widely used today for a variety of purposes.^`
+
+
+const story_6B = 
+    `Martha was getting ready to sing her big solo in the school musical.^
+    Her director gave her very specific instructions for singing the song.^
+    She walked over to her spot on the right side of the stage and sang.^
+    Martha saw one of her fellow actors trip over their feet while dancing.^
+    She was very frazzled by this and then forgot the lyrics to her song.^
+    The song was very fast and there were a lot of words to remember.^
+    She worried that it would be impossible to pick up the song from the middle.^
+    The stage lights were uncomfortably bright and she felt her face get hot.^
+    One of her fellow actors made eye contact with her and saw the panic.^
+    The actor walked to the right side of the stage where Martha was standing.^
+    He mouthed the lyrics of the song until Martha found her place again.^
+    She finished the song and quickly exhaled a massive sigh of relief.^
+    She gave her fellow actor a big hug and thanked him for rescuing the song.^`
+
+
+const story_7B = 
+    `Jeremy was a high school football coach at a small high school.^
+    His team had not had a winning season in nearly an entire decade.^
+    He gathered his players at the ten-yard line to give them a speech.^
+    Jeremy told them that they needed to have more trust in each other.^
+    He told a team captain named Brock to step forward for a demonstration.^
+    He told Brock to get down on his hands and knees for a Death Crawl.^
+    The Death Crawl was a brutal training exercise that players hated.^
+    Brock had to crawl across the field with another player on his back.^
+    Brock got on his hands and knees and another player got on his back.^
+    He started crawling and looked far ahead to the fifty-yard line.^
+    All the other players on the team were cheering him on the whole time.^
+    Brock pushed through the pain until Jeremy told him he could stop.^
+    Jeremy told Brock he had now proven his leadership to the whole team.^`
+
+
+const story_8B = 
+    `Sofia Ionescu is widely considered to be the first female neurosurgeon.^
+    She studied and worked during a time with very few female doctors.^
+    She is best remembered for performing a surgery in Abu Dhabi in 1960.^
+    Her training was very difficult due to the circumstances of World War II.^
+    She got to do surgery partially because there weren't enough male doctors.^
+    She would operate on people when there was no one else qualified to do it.^
+    Sofia Ionescu officially became certified in medicine and surgery in 1945.^
+    Her talents were necessary when the wife of a leader in Abu Dhabi fell ill.^
+    The wife needed brain surgery and needed a female doctor for religious reasons.^
+    Sofia made her famous trip to Abu Dhabi in 1970 and operated on the wife.^
+    The operation was successful and Sofia was given expensive jewelry as a gift.^
+    She returned home to Romania and continued working as a neurosurgeon.^
+    Dr. Sofia Ionescu died in 2008 and is still a widely celebrated role model.^`
+
+
+const story_9B = 
+    `Emily Elizabeth decided she wanted to write a full-length novel for fun.^
+    She was very imaginative and had been writing short stories for years.^
+    She decided to dedicate her winter break to writing her first novel.^
+    Emily Elizabeth sat down with her laptop and opened a blank document.^
+    Somehow all the creativity seemed to have left her body at that moment.^
+    She stared at the blank page and admitted to herself that she had no ideas.^
+    She wrote a few sentences before immediately deleting them and starting over.^
+    Her friend came over to hang out and saw that Emily Elizabeth was frustrated.^
+    The friend suggested that Emily Elizabeth go for a walk to clear her head.^
+    Emily Elizabeth put on her coat and stepped into the cold winter air.^
+    She told her friend that she wanted to write her novel but had no good ideas.^
+    Her friend asked Emily Elizabeth how she came up with short story ideas.^
+    Emily Elizabeth decided to take her favorite short story and make it novel-length.^`
+
+
+const story_10B = 
+    `Mary hadn't seen her good friend Elizabeth since they moved to different cities.^
+    She decided to call Elizabeth and invite her to lunch to catch up on their lives.^
+    Elizabeth was a vegetarian and Mary found a restaurant with plant-based options.^
+    The morning of their catch-up lunch was very cloudy and there was heavy rain.^
+    Mary decided not to wear her nice shoes because of how muddy the ground was.^
+    She started driving and noticed a wasp was flying around near her steering wheel.^
+    She rolled down her windows and frantically tried to shoo the wasp out of the car.^
+    The wasp flew away just as Mary was pulling into the restaurant's parking lot.^
+    She went inside and noticed that Elizabeth was already seated at their booth.^
+    Mary ordered a chicken caesar salad and Elizabeth ordered a pulled pork sandwich.^
+    Mary suddenly remembered she never rolled up her car windows despite the rain.^
+    She ran outside and thanked her lucky stars that the rain had mostly stopped.^
+    She went back inside and had a good laugh about the situation with Elizabeth.^`
+
+
+const story_11B = 
+    `Audrey passionately lived by the phrase 'be the change you want to see in the world.'^
+    She dedicated a lot of time to advocating for causes that were important to her.^
+    She decided that she was going to organize a fundraiser for a pro-life charity.^
+    Audrey gathered her friends in her living room and started asking them for ideas.^
+    Someone suggested that they could organize a charity 5k race to raise money.^
+    Audrey and her friends got down to business and began planning the event.^
+    Her friend Cynthia was in charge of marketing the event to the local community.^
+    Her friend Justin was in charge of managing sign-ups from the participants.^
+    Audrey spent many hours handling all the logistics and scheduling matters.^
+    The group predicted that they'd raise one thousand dollars for pro-life causes.^
+    The race day weather was ideal and there were more than two-hundred runners.^
+    The final fundraising total for the race event was nearly six thousand dollars.^
+    Audrey and her friends delivered a check and a handmade card to the charity.^`
+
+
+const story_12B = 
+    `Mary Pickford was one of the most famous movie stars of the silent era.^
+    She was beloved for playing 'little girl' roles even as an adult woman.^
+    Audiences were big fans of her very long waist-length blonde hair.^
+    Mary was also a clever businesswoman who partnered with other actors.^
+    She helped form a major film distribution company called United Artists.^
+    Another noteworthy member of United Artists was comedian Charlie Chaplin.^
+    Together they made lots of popular movies at the end of the silent era.^
+    Mary Pickford became less popular once talking pictures came along.^
+    She decided she wanted to make a major change in her career and her life.^
+    A hairdresser cut off the very long blonde hair that audiences loved so much.^
+    Mary then starred in the first talking picture version of a Shakespeare play.^
+    Her career never reached its former glory and she retired from acting in 1933.^
+    Mary Pickford was given an Honorary Oscar before her death at age 87.^`
+
+
+const story_13B = 
+    `Rube was a baseball player who became famous for his quirky antics^
+    He played when organized baseball was still a very new sport.^
+    He first caught attention for wearing a bright red bandana on the mound.^
+    Rube was also one of the strongest pitchers playing baseball at the time.^
+    He reliably pitched great games unless he was distracted by something.^
+    Sometimes dogs would bark in the stands and he would lose focus.^
+    Rube even wandered off the field to pet dogs before the games ended.^
+    His antics continued throughout the season and during the off-season.^
+    He once went missing during the winter and was found with a circus.^
+    He performed feats of strength while still wearing his famous green bandana.^
+    One of his memorable acts was wrestling alligators with this traveling circus.^
+    His coach eventually found Rube and made him return to his home.^
+    Rube kept playing baseball until he retired and rejoined the circus.^`
+
+
+
+const condition_b = [
+    {text: story_7B, type: 'spatial small', id: 1},
+    {text: story_1B, type: 'control', id: 2},
+    {text: story_10B, type: 'biographical moral', id: 3},
+    {text: story_6B, type: 'spatial small', id: 4},
+    {text: story_3B, type: 'control', id: 5},
+    {text: story_5B, type: 'spatial grand', id: 6},
+    {text: story_12B, type: 'biographical neutral', id: 7},
+    {text: story_4B, type: 'spatial grand', id: 8},
+    {text: story_2B, type: 'control', id: 9},
+    {text: story_9B, type: 'temporal', id: 10},
+    {text: story_11B, type: 'biographical moral', id: 11},
+    {text: story_8B, type: 'temporal', id: 12},
+    {text: story_13B, type: 'biographical neutral', id: 13}
+    
+]
